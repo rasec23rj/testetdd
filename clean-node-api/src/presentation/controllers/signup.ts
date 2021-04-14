a4b37383ce509fd0f717d1c85a9b53ad51ac235f
jcbarcelos <jcbarcelosrj@gmail.com>
2021-04-14 18:48:26 -0300

    refector: metodo de controller

--- clean-node-api/src/presentation/controllers/signup.ts
+++ clean-node-api/src/presentation/controllers/signup.ts
@@ -1,9 +1,10 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../erros/missing-param-error'
 import { badRequest } from '../helpers/http-helpers'
-export class SignUpController {
+import { Controller } from '../protocols/controller';
+export class SignUpController  implements Controller{
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password']
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for(const field of requiredFields){
         if (!httpRequest.body[field]) {
             return badRequest(new MissingParamError(field))
